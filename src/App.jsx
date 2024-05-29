d5887eda58fd1195b7b89750c4b1eb8871c85544
MhdAliAjaj <engmhdsy630@gmail.com>
2024-05-29 23:12:35 +0300

    first comment

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import Hero from "./components/Hero/Hero"
+import Navbar from "./components/Navbar/Navbar"
+import OurServices from "./components/OurServices/OurServices"
+
+
+const App=()=> {
+  return (
+    <>
+     <Navbar/>
+     <Hero/>
+     <OurServices/>
+    </>
+  )
+}
+
+export default App
